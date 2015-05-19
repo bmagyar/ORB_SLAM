b450a9c2df02842c2a98745e86ac87b87a0f4db4
Han JaeHun <herz1012@gmail.com>
2015-05-19 14:30:01 +0900

    feature2d library link
    when compile this branch, i got some error about FAST, ORB::, etc.. i think that is from opencv library link error so i include all of opencv library.

--- orb_slam/src/ORBextractor.cc
+++ orb_slam/src/ORBextractor.cc
@@ -55,7 +55,7 @@
 *********************************************************************/
 
 
-#include <opencv2/core/core.hpp>
+#include <opencv2/opencv.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <vector>
 
